a120b92a3006658ee0d0e9e44c58a23a29962238
albim72 <marcin.albiniak72@gmail.com>
2023-09-18 15:40:57 +0200

    Create voltage.py

---
+++ DZIEN_1/resistor_project/voltage.py
@@ -0,0 +1,15 @@
+from resistor import Resistor
+
+class VoltageResistance(Resistor):
+    def __init__(self, ohms):
+        super().__init__(ohms)
+        self._voltage = 0
+        
+    @property
+    def voltage(self):
+        return self._voltage
+    
+    @voltage.setter
+    def voltage(self,voltage):
+        self._voltage = voltage
+        self.current = self._voltage/self.ohms
