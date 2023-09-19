f577e79aa1e382de0f6e14d6a78f12f8cd30f3fe
albim72 <marcin.albiniak72@gmail.com>
2023-09-19 10:23:16 +0200

    Update main.py

--- DZIEN_2/egzaminy/main.py
+++ DZIEN_2/egzaminy/main.py
@@ -1,5 +1,6 @@
 from project import Project
 from egzamin import Exam
+from grade import Egzamin
 
 print("_____ klasa Project _____")
 p1 = Project()
@@ -19,3 +20,24 @@ assert g1.python_grade == 70
 assert g1.algo_grade == 72
 
 print(f'oceny: {g1.python_grade}, {g1.algo_grade}')
+
+print("_____ klasa Grade - Egzamin _____")
+
+print("I termin")
+fexam = Egzamin()
+fexam.python_grade = 77
+fexam.algo_grade = 49
+fexam.math_grade = 33
+
+print(f'egzaminy: python {fexam.python_grade}, algotymy {fexam.algo_grade}, matematyka {fexam.math_grade}')
+
+print("II termin")
+sexam = Egzamin()
+sexam.python_grade = 80
+sexam.algo_grade = 51
+sexam.math_grade = 56
+
+print(f'egzaminy: python {sexam.python_grade}, algotymy {sexam.algo_grade}, matematyka {sexam.math_grade}')
+
+print("wyniki pierwszego terminu z archiwum:")
+print(f'egzaminy: python {fexam.python_grade}, algotymy {fexam.algo_grade}, matematyka {fexam.math_grade}')
