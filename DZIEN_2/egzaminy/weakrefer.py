844f571a3e411b2998af45ca150c12630d4d17c3
albim72 <marcin.albiniak72@gmail.com>
2023-09-19 10:31:49 +0200

    Create weakrefer.py

---
+++ DZIEN_2/egzaminy/weakrefer.py
@@ -0,0 +1,20 @@
+from weakref import WeakKeyDictionary
+
+class Grade:
+    def __init__(self):
+        self._values = WeakKeyDictionary()
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return self._values.get(instance,0)
+
+    def __set__(self, instance, value):
+        if not (0 <= value <= 100):
+            raise ValueError('Ocena musi zawierać się w przedziale 0-100')
+        self._values[instance] = value
+
+class Egzamin:
+    python_grade = Grade()
+    algo_grade = Grade()
+    math_grade = Grade()
